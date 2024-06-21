6fdc117f287804994f804a5feb5b6f4f34e35f59
AhmedAlaa21 <ahmedwalidev022@gmail.com>
2024-06-21 23:57:55 +0200

    add paypal packages.

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,11 @@
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+
+const Checkout = () => {
+  return (
+    <div className="py-5" style={{ height: "100%" }}>
+      <p style={{ marginTop: "400px" }}>Checkout</p>
+    </div>
+  );
+};
+
+export default Checkout;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -10,8 +10,15 @@ import HowItWorks from "../pages/HowItWorks";
 import FAQs from "../pages/FAQs";
 import MySmartersAccount from "../pages/MySmartersAccount";
 import Blog from "../pages/Blog";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Checkout from "../pages/Checkout";
 
 const AppRouter = () => {
+  const initialOptions = {
+    clientId: "YOUR-CLIENT-ID-HERE",
+    currency: "USD",
+    intent: "capture",
+  };
   const router = createBrowserRouter([
     {
       path: "/",
@@ -53,6 +60,14 @@ const AppRouter = () => {
         {
           path: "account",
           element: <MySmartersAccount />,
+        },
+        {
+          path: "checkout",
+          element: (
+            <PayPalScriptProvider options={initialOptions}>
+              <Checkout />
+            </PayPalScriptProvider>
+          ),
         },
       ],
     },
